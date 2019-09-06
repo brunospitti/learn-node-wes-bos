a6032a0b47f40857ebec692b80b48e12012f5a55
Bruno Spitti <bspitti@cartrawler.com>
2019-09-06 17:32:48 +0100

    removing some shit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey!ad asdasd asdsdsd It works!');
+  res.send('Hey! It works!');
 });
 
 module.exports = router;
