28aace445cc2bab0003ef9de1407ae9f2eaf0acf
Bruno Spitti <bspitti@cartrawler.com>
2019-09-10 16:53:39 +0100

    Video 08

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController')
+
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
-});
+router.get('/', storeController.myMiddleware, storeController.homePage);
+
+router.get('/reverse/:name', (req, res) => {
+  const reversedName = [...req.params.name].reverse().join('')
+  res.send(reversedName)
+})
 
 module.exports = router;
